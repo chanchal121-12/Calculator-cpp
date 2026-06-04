fec063d5b95d04af9dc51a7894a00060658c4273
Chanchal Rai <chanchalrai411@gmail.com>
2026-06-04 11:20:36 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op)
+    {
+        case '+':
+            cout << "Result = " << a + b;
+            break;
+
+        case '-':
+            cout << "Result = " << a - b;
+            break;
+
+        case '*':
+            cout << "Result = " << a * b;
+            break;
+
+        case '/':
+            cout << "Result = " << (float)a / b;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
